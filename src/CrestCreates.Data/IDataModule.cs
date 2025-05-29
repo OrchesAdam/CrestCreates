6058fba5a2abf7c43e8301976dd3f68a661eca30
Orches <10153835+OrchesAdam@users.noreply.github.com>
2025-05-29 11:49:26 +0800

    refactor(data): 将 DataModuleOptions 类移动到单独的文件中
    - 将 DataModuleOptions 类从 IDataModule.cs 文件中分离出来
    - 在新的 DataModuleOptions.cs 文件中定义 DataModuleOptions 类- 在 IDataModule 接口中添加 ModuleInterface 属性，指定配置类型

--- src/CrestCreates.Data/IDataModule.cs
+++ src/CrestCreates.Data/IDataModule.cs
@@ -5,39 +5,9 @@ using CrestCreates.Modularity;
 namespace CrestCreates.Data;
 
 /// <summary>
-/// 数据模块配置选项
-/// </summary>
-public class DataModuleOptions
-{
-    /// <summary>
-    /// 数据库连接字符串
-    /// </summary>
-    public string ConnectionString { get; set; } = string.Empty;
-    
-    /// <summary>
-    /// 是否自动执行迁移
-    /// </summary>
-    public bool AutoMigrate { get; set; } = false;
-    
-    /// <summary>
-    /// 数据库类型
-    /// </summary>
-    public string DatabaseType { get; set; } = "SqlServer";
-    
-    /// <summary>
-    /// 命令超时时间（秒）
-    /// </summary>
-    public int CommandTimeout { get; set; } = 30;
-    
-    /// <summary>
-    /// 是否启用连接池
-    /// </summary>
-    public bool EnableConnectionPooling { get; set; } = true;
-}
-
-/// <summary>
 /// 数据模块接口
 /// </summary>
+[ModuleInterface(ConfigurationType = typeof(DataModuleOptions))]
 public interface IDataModule : ICrestCreatesModule
 {
     /// <summary>
