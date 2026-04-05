e83402532462478850f6dfab355afec94122866e
Orches <10153835+OrchesAdam@users.noreply.github.com>
2026-04-05 10:12:14 +0800

    feat(数据过滤): 添加数据过滤接口和状态管理
    添加IDisableDataFilter和IDataPermissionFilter接口用于数据过滤功能
    实现DataFilterState类管理过滤状态
    删除不再使用的AutoMapper配置类

---
+++ framework/src/CrestCreates.Domain.Shared/DataFilter/IDisableDataFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrestCreates.Domain.DataFilter;
+
+public interface IDisableDataFilter
+{
+    bool IsEnabled<TFilter>() where TFilter : class;
+
+    IDisposable Disable<TFilter>() where TFilter : class;
+
+    IDisposable Enable<TFilter>() where TFilter : class;
+}
--- framework/src/CrestCreates.Domain/DataFilter/DataFilterState.cs
+++ framework/src/CrestCreates.Domain/DataFilter/DataFilterState.cs
@@ -3,15 +3,6 @@ using System.Collections.Generic;
 
 namespace CrestCreates.Domain.DataFilter
 {
-    public interface IDisableDataFilter
-    {
-        bool IsEnabled<TFilter>() where TFilter : class;
-
-        IDisposable Disable<TFilter>() where TFilter : class;
-
-        IDisposable Enable<TFilter>() where TFilter : class;
-    }
-
     public class DataFilterState
     {
         public Dictionary<Type, bool> FilterStates { get; } = new Dictionary<Type, bool>();
