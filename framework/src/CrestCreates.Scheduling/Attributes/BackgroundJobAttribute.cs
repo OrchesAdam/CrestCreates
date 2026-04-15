62cf97215839c458ebfb041a950c5fe1a7a4c213
Orches <10153835+OrchesAdam@users.noreply.github.com>
2026-04-15 17:26:57 +0800

    feat(scheduling): add BackgroundJobAttribute for job discovery

---
+++ framework/src/CrestCreates.Scheduling/Attributes/BackgroundJobAttribute.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace CrestCreates.Scheduling.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class BackgroundJobAttribute : Attribute
+{
+    /// <summary>Job display name. Defaults to class name.</summary>
+    public string? Name { get; init; }
+
+    /// <summary>Cron expression for recurring jobs. If empty/null, job is one-time/delayed.</summary>
+    public string? CronExpression { get; init; }
+
+    /// <summary>Whether authorization is required. Default false.</summary>
+    public bool EnableAuthorization { get; init; }
+
+    /// <summary>Job group name. Default "Default".</summary>
+    public string Group { get; init; } = "Default";
+}
