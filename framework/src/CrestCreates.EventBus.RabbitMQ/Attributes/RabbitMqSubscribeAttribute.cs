3531ce02f99fe2b76bee5411ba5e0811bf720935
orches <OrchesAdam@outlook.com>
2026-04-27 00:39:31 +0800

    feat(eventbus-rabbitmq): add RabbitMqSubscribe attribute

---
+++ framework/src/CrestCreates.EventBus.RabbitMQ/Attributes/RabbitMqSubscribeAttribute.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CrestCreates.EventBus.RabbitMQ.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public sealed class RabbitMqSubscribeAttribute : Attribute
+{
+    public string EventType { get; }
+    public string? Exchange { get; set; }
+    public string? Queue { get; set; }
+    public int PrefetchCount { get; set; } = 10;
+
+    public RabbitMqSubscribeAttribute(string eventType)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(eventType);
+        EventType = eventType;
+    }
+}
