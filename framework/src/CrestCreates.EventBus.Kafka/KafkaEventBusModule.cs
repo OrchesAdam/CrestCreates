14311a05d0b37ebc636a729994c56ccd10e2448f
orches <OrchesAdam@outlook.com>
2026-04-27 15:03:21 +0800

    feat(eventbus-kafka): add module definition

--- framework/src/CrestCreates.EventBus.Kafka/KafkaEventBusModule.cs
+++ framework/src/CrestCreates.EventBus.Kafka/KafkaEventBusModule.cs
@@ -1,29 +1,13 @@
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 using CrestCreates.Modularity;
-using CrestCreates.EventBus.Kafka.Options;
-using CrestCreates.EventBus.Abstract;
 
 namespace CrestCreates.EventBus.Kafka;
 
-/// <summary>
-/// Module for configuring Kafka distributed event bus.
-/// </summary>
 public class KafkaEventBusModule : ModuleBase
 {
-    private readonly IConfiguration _configuration;
-
-    public KafkaEventBusModule(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
     public override void OnConfigureServices(IServiceCollection services)
     {
-        // Configure options
-        services.Configure<KafkaOptions>(_configuration.GetSection("Kafka"));
-
-        // Register event bus
-        services.AddSingleton<IEventBus, KafkaEventBus>();
+        // Module provides defaults - user should call AddKafkaEventBus<TContext>
+        // in their startup with their specific JsonSerializerContext
     }
 }
